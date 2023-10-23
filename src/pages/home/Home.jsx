deb2a745d25e0c6c49a7635b31c7637aa6fd19ee
unknown <yazid1bernou@gmail.com>
2023-10-23 13:47:25 +0000

    create featured layout in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Home.scss';
+import Featured from '../../components/featured/Featured';
 const Home = () => {
   return (
     <div>
-          Home page
+          <Featured />
     </div>
   )
 }

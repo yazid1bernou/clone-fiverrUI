91bda3e6a1ac0e4e8aaf96d3887e2f044a256db4
unknown <yazid1bernou@gmail.com>
2023-10-24 12:51:13 +0000

    we continued the slide part

--- src/components/catCard/CatCard.jsx
+++ src/components/catCard/CatCard.jsx
@@ -5,11 +5,11 @@ const CatCard = ({item}) => {
   return (
     <Link to="/gigs?cat=design">
       <div className='catCard'>
-            <div className="container">
+            
                 <img src={item.img} alt="" />
                 <span className='desc'>{item.desc}</span>
                 <span className='title'>{item.title}</span>
-            </div>
+         
       </div>
     </Link>
   )

d87c69fe7210b920ad3d6c547e7c5cf6047b7d14
unknown <yazid1bernou@gmail.com>
2023-10-25 22:39:23 +0200

    create Gig Card

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -6,6 +6,26 @@ const GigCart = ({item}) => {
         
         <div className="gigCard">
               <img src={item.img} alt="" />
+              <div className="info">
+                   <div className="user">
+                      <img src={item.pp} alt="" />
+                      <span>{item.username}</span>
+                   </div>
+                   <p>{item.desc}</p>
+                   <div className="star">
+                    <img src="./img/star.png" alt="" />
+                    <span>{item.star}</span>
+                   </div>
+              </div>
+              <hr/>
+              <div className="details">
+                  <img src="./img/heart.png" alt="" />
+                  <div className="price">
+                    <span>STARTING AT</span>
+                    <h2>${item.price}</h2>
+                  </div>
+                  
+              </div>
         </div>
 
         </Link>

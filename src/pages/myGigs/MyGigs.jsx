f521addb5ef9283754aee63452051fbf186874d6
unknown <yazid1bernou@gmail.com>
2023-11-09 13:12:41 +0000

    finiched the structure and style of Mygigs page

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -22,13 +22,68 @@ const MyGigs = () => {
               </tr>
               <tr>
                 <td>
-                  <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/51c35c7cecf75e6a5a0110d27909a2f5-1690202609364/EN.png" alt="" />
+                  <img className="img" src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 </td>
                 <td>Gig1</td>
                 <td>88</td>
                 <td>123</td>
                 <td>
-                   <img src="/img/delete.png" alt="" />
+                   <img className="delete" src="/img/delete.png" alt="" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img className="img" src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>88</td>
+                <td>123</td>
+                <td>
+                   <img className="delete" src="/img/delete.png" alt="" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img className="img" src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>88</td>
+                <td>123</td>
+                <td>
+                   <img className="delete" src="/img/delete.png" alt="" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img className="img" src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>88</td>
+                <td>123</td>
+                <td>
+                   <img className="delete" src="/img/delete.png" alt="" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img className="img" src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>88</td>
+                <td>123</td>
+                <td>
+                   <img className="delete" src="/img/delete.png" alt="" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img className="img" src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>88</td>
+                <td>123</td>
+                <td>
+                   <img className="delete" src="/img/delete.png" alt="" />
                 </td>
               </tr>
 

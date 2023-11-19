1650ca62b67b032a2c4dbc818a31af3cc336a899
unknown <yazid1bernou@gmail.com>
2023-11-19 09:20:36 +0000

    completed the styles of Message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -15,8 +15,32 @@ const Message = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi architecto soluta. Consequuntur, officiis quisquam! Quae culpa ratione nemo molestiae recusandae. Quasi minima eaque laudantium magnam repudiandae similique est rem?
                  </p>
              </div>
-
+             <div className="item">
+                 <img src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                 <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi architecto soluta. Consequuntur, officiis quisquam! Quae culpa ratione nemo molestiae recusandae. Quasi minima eaque laudantium magnam repudiandae similique est rem?
+                 </p>
+             </div>
+             <div className="item owner">
+                 <img src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                 <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi architecto soluta. Consequuntur, officiis quisquam! Quae culpa ratione nemo molestiae recusandae. Quasi minima eaque laudantium magnam repudiandae similique est rem?
+                 </p>
+             </div>
+             <div className="item">
+                 <img src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                 <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi architecto soluta. Consequuntur, officiis quisquam! Quae culpa ratione nemo molestiae recusandae. Quasi minima eaque laudantium magnam repudiandae similique est rem?
+                 </p>
+             </div>
+             <div className="item owner">
+                 <img src="https://images.pexels.com/photos/2079246/pexels-photo-2079246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                 <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi architecto soluta. Consequuntur, officiis quisquam! Quae culpa ratione nemo molestiae recusandae. Quasi minima eaque laudantium magnam repudiandae similique est rem?
+                 </p>
+             </div>
           </div>
+          <hr />
           <div className="write">
             <textarea name="" placeholder='Write a message' id="" cols="30" rows="10"></textarea>
             <button>Send</button>
